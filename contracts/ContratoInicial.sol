1ba0e64d172e4223937aa3e7e03bc9b68f6b77f8
enrique soteldo <soteldo18@hotmail.com>
2022-12-22 16:21:48 -0400

    funcion desadoptar

--- contracts/ContratoInicial.sol
+++ contracts/ContratoInicial.sol
@@ -17,6 +17,19 @@ contract ContratoInicial {
 
   }
 
+  function desadoptar(uint perroIndex) public returns(bool){
+    require(perroIndex >=0 && perroIndex<= 15,"Fuera de Rango");
+    bool desAdoptarConExito=true;
+  
+    if(perrosAdoptados[perroIndex] ==msg.sender){
+      perrosAdoptados[perroIndex]= address(0);
+    }else{
+      desAdoptarConExito=false;
+    }
+    return desAdoptarConExito;
+
+  }
+
   function getPerrosAdoptados() public view returns (address[16] memory){
     return perrosAdoptados;
   }
